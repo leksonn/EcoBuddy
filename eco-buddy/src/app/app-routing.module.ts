7649232fdad84c2867a7fda2386b7a586b22b9b9
ahodzicc <amna.hodzic@stu.ssst.edu.ba>
2025-02-22 15:25:16 +0100

    fixes to routing

--- eco-buddy/src/app/app-routing.module.ts
+++ eco-buddy/src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { GamaComponent } from './gama/gama.component';
 
 const routes: Routes = [
-
   { path: '', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-
+  { path: 'gamification', component: GamaComponent },
 ];
 
 @NgModule({
--- eco-buddy/src/app/app.module.ts
+++ eco-buddy/src/app/app.module.ts
@@ -1,24 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; // Required for two-way data binding
-import { AppRoutingModule } from './app-routing.module'; // Import the routing module
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-
+import { GamaComponent } from './gama/gama.component';
+ import { GamificationService } from './gama/gama.service';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignupComponent,
-
+    GamaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+
   ],
-  providers: [],
+  providers: [GamificationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
